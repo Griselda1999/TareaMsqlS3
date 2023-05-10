e6f0a076f1b414fd8e035545df526c6d0710f9b9
Griselda1999 <116777541+Griselda1999@users.noreply.github.com>
2023-05-09 22:40:54 -0600

    Add files via upload

--- ComandosRegistroAlumnos.sql
+++ ComandosRegistroAlumnos.sql
@@ -1,12 +1,15 @@
+-- Registro de Alumnos  tres de las tablas tienen los 3 comandos que se 
+-- solicitaron y las otras 5 solotiene el comando Insert.
+
 -- TABLA CAMPUS
--- Insert
+-- Insert para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_campus` (`id`, `nombre`, `ubicacion`) VALUES ('1', 'Ceutec Centroamerica', 'Bulevar Centroamerica');
 INSERT INTO `bd_registroalumno`.`tbl_campus` (`id`, `nombre`, `ubicacion`) VALUES ('2', 'Ceutec La Ceiba', 'La Ceiba');
 INSERT INTO `bd_registroalumno`.`tbl_campus` (`id`, `nombre`, `ubicacion`) VALUES ('3', 'Ceutec Proceres', 'Los Proceres');
 INSERT INTO `bd_registroalumno`.`tbl_campus` (`id`, `nombre`, `ubicacion`) VALUES ('4', 'Ceutec Prado', 'El prado');
 INSERT INTO `bd_registroalumno`.`tbl_campus` (`id`, `nombre`, `ubicacion`) VALUES ('5', 'Ceutec San pedro', 'Cortes');
 
--- Update
+-- Update para actualiza los campos de las tablas
 UPDATE `bd_registroalumno`.`tbl_campus` SET `ubicacion` = 'Q696+34Q, Carr. a Trujillo, La Ceiba' WHERE (`id` = '2');
 UPDATE `bd_registroalumno`.`tbl_campus` SET `ubicacion` = '3RM8+82Q, Tegucigalpa' WHERE (`id` = '1');
 UPDATE `bd_registroalumno`.`tbl_campus` SET `ubicacion` = '4R37+MHV, Tegucigalpa' WHERE (`id` = '3');
@@ -15,7 +18,7 @@ UPDATE `bd_registroalumno`.`tbl_campus` SET `ubicacion` = 'GXJ9+35X, Bulevar UNA
 UPDATE `bd_registroalumno`.`tbl_campus` SET `id` = '7' WHERE (`id` = '3');
 UPDATE `bd_registroalumno`.`tbl_campus` SET `id` = '8' WHERE (`id` = '2');
 
--- Delete 
+-- Delete para eliminar los capos de las tablas
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '4');
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '3');
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '1');
@@ -23,38 +26,38 @@ DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '1');
 
 
 -- TABLA MATERIA
--- Insert
+-- Insert  para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_materia` (`id`, `nombre`, `descripcion`) VALUES ('1',                ' Matematicas', ' Diferencia entre problema y ejercicio matemático.');
 INSERT INTO `bd_registroalumno`.`tbl_materia` (`id`, `nombre`, `descripcion`) VALUES ('2', 'Español', ' Aprender las diferencias entre subjuntivo e indicativo.');
 INSERT INTO `bd_registroalumno`.`tbl_materia` (`id`, `nombre`, `descripcion`) VALUES ('3',    'Ingles', ' Comunicarse en el idioma inglés, incluyendo saludos simples. ');
 INSERT INTO `bd_registroalumno`.`tbl_materia` (`id`, `nombre`, `descripcion`) VALUES ('4',        'Base de Datos', ' Colección de informaciones homogéneas.');
 INSERT INTO `bd_registroalumno`.`tbl_materia` (`id`, `nombre`, `descripcion`) VALUES ('5', 'Historia', 'La historia de honduras.');
 
--- Update 
+-- Update para actualiza los campos de las tablas
 UPDATE `bd_registroalumno`.`tbl_materia` SET `nombre` = 'Algebra' WHERE (`id` = '1');
 UPDATE `bd_registroalumno`.`tbl_materia` SET `nombre` = 'Teoria de Base de Datos' WHERE (`id` = '4');
 UPDATE `bd_registroalumno`.`tbl_materia` SET `nombre` = 'Historia de Honduras' WHERE (`id` = '5');
 UPDATE `bd_registroalumno`.`tbl_materia` SET `id` = '6' WHERE (`id` = '4');
 
--- Delete 
+-- Delete para eliminar los capos de las tablas
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '2'); 
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '3');
 DELETE FROM `bd_registroalumno`.`tbl_materia` WHERE (`id` = '1');                                                                                                                            
 
 -- TABLA DOCENTE
--- Insert
+-- Insert  para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_docente` (`id`, `nombre`, `apellido`, `correo`) VALUES ('1', 'Francisco', 'Núñez', 'nuñezfrancisco@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_docente` (`id`, `nombre`, `apellido`, `correo`) VALUES ('2', 'Cristina', 'Sosa', 'Csosa@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_docente` (`id`, `nombre`, `apellido`, `correo`) VALUES ('3', 'Julio', 'Laínez', 'juliolainez@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_docente` (`id`, `nombre`, `apellido`, `correo`) VALUES ('4', 'Ana', 'Loredo', 'Loredo32@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_docente` (`id`, `nombre`, `apellido`, `correo`) VALUES ('5', 'Fernando', 'Rosales', 'RosalesFer@gmail.com');
 
--- Udate 
+-- Udate para actualiza los campos de las tablas
 UPDATE `bd_registroalumno`.`tbl_docente` SET `id` = '7' WHERE (`id` = '2');
 UPDATE `bd_registroalumno`.`tbl_docente` SET `apellido` = 'Rodriguez' WHERE (`id` = '3');
 UPDATE `bd_registroalumno`.`tbl_docente` SET `id` = '9' WHERE (`id` = '1');
 
--- Delete 
+-- Delete para eliminar los capos de las tablas
 DELETE FROM `bd_registroalumno`.`tbl_docente` WHERE (`id` = '4');
 DELETE FROM `bd_registroalumno`.`tbl_docente` WHERE (`id` = '1');
 DELETE FROM `bd_registroalumno`.`tbl_docente` WHERE (`id` = '3');
@@ -62,7 +65,7 @@ DELETE FROM `bd_registroalumno`.`tbl_docente` WHERE (`id` = '3');
 
 
 -- TABLA SECCION
--- Insert
+-- Insert  para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_seccion` (`id`, `num_aula`, `id_campus`, `id_docente`, `id_materia`) VALUES ('1', 'AT5', '2', '3', '1');
 INSERT INTO `bd_registroalumno`.`tbl_seccion` (`id`, `num_aula`, `id_campus`, `id_docente`, `id_materia`) VALUES ('2', 'ET4', '2', '2', '3');
 INSERT INTO `bd_registroalumno`.`tbl_seccion` (`id`, `num_aula`, `id_campus`, `id_docente`, `id_materia`) VALUES ('3', 'ST4', '3', '4', '2');
@@ -71,7 +74,7 @@ INSERT INTO `bd_registroalumno`.`tbl_seccion` (`id`, `num_aula`, `id_campus`, `i
 INSERT INTO `bd_registroalumno`.`tbl_seccion` (`id`, `num_aula`, `id_campus`, `id_docente`, `id_materia`) VALUES ('6', 'AT8', '1', '5', '5');
 
 -- TABLA ASISTE
--- Insert
+-- Insert para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_asiste` (`id`, `id_seccion`, `id_alumno`) VALUES ('1', '2', '1');
 INSERT INTO `bd_registroalumno`.`tbl_asiste` (`id`, `id_seccion`, `id_alumno`) VALUES ('2', '1', '3');
 INSERT INTO `bd_registroalumno`.`tbl_asiste` (`id`, `id_seccion`, `id_alumno`) VALUES ('3', '1', '5');
@@ -79,7 +82,7 @@ INSERT INTO `bd_registroalumno`.`tbl_asiste` (`id`, `id_seccion`, `id_alumno`) V
 INSERT INTO `bd_registroalumno`.`tbl_asiste` (`id`, `id_seccion`, `id_alumno`) VALUES ('5', '5', '3');
 
 -- TABLA ALUMNO
--- Insert
+-- Insert para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_alumno` (`id`, `nombre`, `apellido`, `correo`) VALUES ('1', 'Lourdes', 'Sanchez', 'losan21@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_alumno` (`id`, `nombre`, `apellido`, `correo`) VALUES ('2', 'David', 'Gutierrez', 'gutierres@gmail.com');
 INSERT INTO `bd_registroalumno`.`tbl_alumno` (`id`, `nombre`, `apellido`, `correo`) VALUES ('3', 'Josue', 'Ramos', 'jramos@gmail.com');
@@ -88,7 +91,7 @@ INSERT INTO `bd_registroalumno`.`tbl_alumno` (`id`, `nombre`, `apellido`, `corre
 
 
 -- TABLA CARRERA
--- Insert
+-- Insert  para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_carrera` (`id`, `nombre`, `num_clases`, `duracion`) VALUES ('1', 'Ingenieria en sistemas ', '54', '4 años');
 INSERT INTO `bd_registroalumno`.`tbl_carrera` (`id`, `nombre`, `num_clases`, `duracion`) VALUES ('2', 'Ingenieria Civil', '52', '4 años');
 INSERT INTO `bd_registroalumno`.`tbl_carrera` (`id`, `nombre`, `num_clases`, `duracion`) VALUES ('3', 'Desarrollo Web', '26', '2 años');
@@ -97,7 +100,7 @@ INSERT INTO `bd_registroalumno`.`tbl_carrera` (`id`, `nombre`, `num_clases`, `du
 
 
 -- TABLA MATRICULA
--- Insert
+-- Insert  para insertar informacion a los campos de las tablas
 INSERT INTO `bd_registroalumno`.`tbl_matricula` (`id`, `id_carrera`, `id_alumno`) VALUES ('1', '2', '1');
 INSERT INTO `bd_registroalumno`.`tbl_matricula` (`id`, `id_carrera`, `id_alumno`) VALUES ('2', '1', '4');
 INSERT INTO `bd_registroalumno`.`tbl_matricula` (`id`, `id_carrera`, `id_alumno`) VALUES ('3', '3', '1');
